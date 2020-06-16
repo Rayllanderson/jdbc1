9a2b91679f5bdca22cbd292d961dd5418bb05c27
Rayllanderson <rayllanderson@gmail.com>
2020-06-16 13:49:12 -0300

    pegando dados com resultSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
     public static void main(String[] args) {
-	
-	Connection coon = DB.getConnection();
-	DB.closeConnection();
-	
+
+	Connection coon = null;
+	Statement st = null;
+	ResultSet rs = null;
+
+	try {
+	    coon = DB.getConnection();
+
+	    st = coon.createStatement();
+
+	    rs = st.executeQuery("select * from department");
+
+	    while (rs.next()) {
+		// pega o inteiro que ta no campo 'id':
+		System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (rs != null) {
+		DB.closeResultSet(rs);
+		DB.closeStatement(st);
+		DB.closeConnection();
+	    }
+	}
     }
 }
